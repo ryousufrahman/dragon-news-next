733e50236df3762ff6983552af9ce6460f004344
yousufrahman <ryousuf766@gmail.com>
2026-04-28 00:05:16 +0600

    added the layout for category and news and social link

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -3,8 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-     <div>
-       <h2>home page</h2>
+     <div className=" container mx-auto grid grid-cols-12 gap-4 mt-4">
+       <div className=" bg-green-300 col-span-3">
+        left side
+       </div>
+       <div className="bg-red-300 col-span-6">
+        middle
+       </div>
+       <div className="bg-blue-300 col-span-3">
+        right
+       </div>
+       
      </div>
     
     
